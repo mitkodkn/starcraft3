a65a72915579eff9687dcbdd9eb6bd3d299fa7f6
dknikolov@gmail.com <dimitar@doonloo.com>
2021-12-03 03:01:25 +0200

    Improvements

--- main.c
+++ main.c
@@ -1,41 +1,50 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <unistd.h>
+//--------------------------------------------
+// NAME: Dimitar Nikolov
+// CLASS: XIa
+// NUMBER: 7
+// PROBLEM: #3
+// FILE NAME: main.c
+// FILE PURPOSE:
+//	The whole game in one file
+//---------------------------------------------
+
 #include <pthread.h>
-#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
 
 typedef struct mineral_block {
-	unsigned int minerals;
+	int minerals;
 	pthread_mutex_t digging;
 } mineral_block_t;
 
 typedef struct map {
-	unsigned int minerals;
-	unsigned int mineral_blocks_count;
+	int start_minerals;
+	int minerals;
+	int mineral_blocks_count;
 	pthread_mutex_t digging;
 	mineral_block_t *mineral_blocks;
 } map_t;
 
 typedef struct command_center {
-	unsigned int scv;
-	unsigned int marines;
-	unsigned int minerals;
+	int scv;
+	int marines;
+	int minerals;
 	pthread_t *scvs;
 	pthread_mutex_t active;
 } command_center_t;
 
 struct arguments {
-	unsigned int id;
+	int id;
 	map_t *map;
 	command_center_t *center;
 };
 
-
 int initialize_map(map_t*, const int);
 int initialize_center(command_center_t*);
-bool has_game_ended(command_center_t*, map_t*);
 void construct_marine(command_center_t*);
 int train_scv(command_center_t*, map_t*);
 
@@ -52,7 +61,6 @@ int main(int argc, char const *argv[]) {
 	if (error != 0) {
 		return 1;
 	}
-
 
 	command_center_t center;
 	error = initialize_center(&center);
@@ -86,10 +94,7 @@ int main(int argc, char const *argv[]) {
 		return 1;
 	}
 
-
-	// ------------------------------------------------------------
-
-	for (unsigned int scv_id = 0; scv_id < center.scv; scv_id++) {
+	for (int scv_id = 0; scv_id < center.scv; scv_id++) {
 		struct arguments *dig_args = (struct arguments*) malloc(sizeof(struct arguments));
 
 		if (dig_args == NULL) {
@@ -108,23 +113,38 @@ int main(int argc, char const *argv[]) {
 		}
 	}
 
-	// ------------------------------------------------------------
-
 	error = pthread_join(controller, NULL);
 	if (error != 0) {
 		perror("pthread_join");
 		return 1;
 	}
 
-	for (unsigned int scv_id = 0; scv_id < center.scv; scv_id++) {
+	for (int scv_id = 0; scv_id < center.scv; scv_id++) {
 		error = pthread_join(scvs[scv_id], NULL);
 		if (error != 0) {
-			perror("pthread_join");
+			fprintf(stderr, "%d: %s\n", scv_id, strerror(errno));
 			return 1;
 		}
 	}
 
-	printf("Mineral blocks on map %d\nMineral blocks in center: %d\n", map.minerals, center.minerals);
+	printf("Map minerals %d, player minerals %d, SCVs %d, Marines %d\n", map.start_minerals, center.minerals, center.scv, center.marines);
+
+	for (int i = 0; i < map.mineral_blocks_count; i++) {
+		error = pthread_mutex_destroy(&map.mineral_blocks[i].digging);
+		if (error != 0) {
+			perror("pthread_mutex_destroy");
+			return 1;
+		}
+	}
+
+	error = pthread_mutex_destroy(&map.digging);
+	if (error != 0) {
+		perror("pthread_mutex_destroy");
+		return 1;
+	}
+
+	free(map.mineral_blocks);
+	free(scvs);
 
 	return 0;
 }
@@ -140,7 +160,7 @@ int initialize_map(map_t* map, const int blocks) {
 		return 1;
 	}
 
-	map->minerals = map->mineral_blocks_count * 500; // constant
+	map->start_minerals = map->minerals = map->mineral_blocks_count * 500; // constant
 
 	error = pthread_mutex_init(&map->digging, NULL);
 
@@ -149,7 +169,7 @@ int initialize_map(map_t* map, const int blocks) {
 		return 1;
 	}
 
-	for (unsigned int i = 0; i < map->mineral_blocks_count; i++) {
+	for (int i = 0; i < map->mineral_blocks_count; i++) {
 		map->mineral_blocks[i].minerals = 500; // constant
 		error = pthread_mutex_init(&map->mineral_blocks[i].digging, NULL);
 
@@ -179,25 +199,6 @@ int initialize_center(command_center_t *center) {
 	return 0;
 }
 
-bool has_game_ended(command_center_t *center, map_t *map) {
-	while (pthread_mutex_trylock(&map->digging));
-	while (pthread_mutex_trylock(&center->active));
-
-	if (center->marines >= 20 || map->minerals <= 0 || center->marines + center->scv >= 200) {
-		center->marines = 20;
-		map->minerals = 0;
-		pthread_mutex_unlock(&map->digging);
-		pthread_mutex_unlock(&center->active);
-		return true;
-	}
-
-	pthread_mutex_unlock(&map->digging);
-	pthread_mutex_unlock(&center->active);
-
-	return false;
-}
-
-
 void *control(void* arg) {
 	int error;
 
@@ -206,20 +207,22 @@ void *control(void* arg) {
 
 	char buffer[2];
 
-	while (!has_game_ended(args.center, args.map)) {
+	while (args.center->marines < 20) {
 
 		fgets(buffer, 2, stdin);
 
-		if (strcmp(buffer, "m") == 0 && !has_game_ended(args.center, args.map)) {
+		if (strcmp(buffer, "m") == 0) {
 			while (pthread_mutex_trylock(&args.center->active));
 			construct_marine(args.center);
 			pthread_mutex_unlock(&args.center->active);
-		} else if (strcmp(buffer, "s") == 0 && !has_game_ended(args.center, args.map)) {
+		} else if (strcmp(buffer, "s") == 0) {
+			while (pthread_mutex_trylock(&args.center->active));
 			error = train_scv(args.center, args.map);
 
 			if (error != 0) {
 				return (void*)1;
 			}
+			pthread_mutex_unlock(&args.center->active);
 		}
 	}
 
@@ -232,38 +235,39 @@ void *dig(void* arg) {
 	struct arguments *args_p = (struct arguments*) arg;
 	struct arguments args = *args_p;
 
-	while (!has_game_ended(args.center, args.map)) {
-		for (unsigned int i = 0; i < args.map->mineral_blocks_count && !has_game_ended(args.center, args.map); i++) {
+	int amount_added = 0;
+
+	while (args.map->minerals > 0) {
+		for (int i = 0; i < args.map->mineral_blocks_count && args.map->minerals > 0; i++) {
 			if (args.map->mineral_blocks[i].minerals > 0) {
 //				sleep(3);
 				if (pthread_mutex_trylock(&args.map->mineral_blocks[i].digging) == 0) {
 					printf("SCV %d is mining from mineral block %d\n", args.id + 1, i + 1);
 
-					if (args.map->mineral_blocks[i].minerals - 8 <= 0) {
-						args.map->mineral_blocks[i].minerals = 0;
-					} else {
-						args.map->mineral_blocks[i].minerals -= 8;
+					if (args.map->mineral_blocks[i].minerals > 0) {
+						if (args.map->mineral_blocks[i].minerals - 8 <= 0) {
+							amount_added = args.map->mineral_blocks[i].minerals;
+							args.map->mineral_blocks[i].minerals = 0;
+						} else {
+							amount_added = 8;
+							args.map->mineral_blocks[i].minerals -= amount_added;
+						}
+
+						pthread_mutex_unlock(&args.map->mineral_blocks[i].digging);
+
+						while (pthread_mutex_trylock(&args.map->digging));
+						args.map->minerals -= amount_added;
+						pthread_mutex_unlock(&args.map->digging);
+
+						printf("SCV %d is transporting minerals\n", args.id + 1);
+	//					sleep(2);
+
+						while (pthread_mutex_trylock(&args.center->active));
+						args.center->minerals += amount_added;
+						printf("adding minerals...\n new amount: %d\n", args.center->minerals);
+						pthread_mutex_unlock(&args.center->active);
+						printf("SCV %d delivered minerals to the Command center\n", args.id + 1);
 					}
-
-					if (args.map->minerals - 8 <= 0) {
-						args.map->minerals = 0;
-					} else {
-						args.map->minerals -= 8;
-					}
-
-					pthread_mutex_unlock(&args.map->mineral_blocks[i].digging);
-
-					if (has_game_ended(args.center, args.map)) {
-						break;
-					}
-
-					printf("SCV %d is transporting minerals\n", args.id + 1);
-//					sleep(2);
-
-					while (pthread_mutex_trylock(&args.center->active));
-					args.center->minerals += 8;
-					pthread_mutex_unlock(&args.center->active);
-					printf("SCV %d delivered minerals to the Command center\n", args.id + 1);
 				}
 			}
 		}
@@ -276,9 +280,11 @@ void *dig(void* arg) {
 
 void construct_marine(command_center_t *center) {
 	if (center->minerals >= 50) {
-		sleep(1);
+//		sleep(1);
 		center->minerals -= 50;
+		printf("removing minerals...\n new amount: %d\n", center->minerals);
 		center->marines += 1;
+		printf("adding marine...\n new amount: %d\n", center->marines);
 		printf("You wanna piece of me, boy?\n");
 	} else {
 		printf("Not enough minerals.\n");
@@ -289,9 +295,11 @@ int train_scv(command_center_t *center, map_t *map) {
 	int error;
 
 	if (center->minerals >= 50) {
-		sleep(4);
+//		sleep(4);
 		center->minerals -= 50;
 		center->scv += 1;
+//		printf("removing minerals...\n new amount: %d\n", center->minerals);
+//		printf("adding scv...\n new amount: %d\n", center->scv);
 
 		center->scvs = (pthread_t*) realloc(center->scvs, sizeof(pthread_t) * center->scv);
 
@@ -310,9 +318,8 @@ int train_scv(command_center_t *center, map_t *map) {
 		dig_args->id = center->scv - 1;
 		dig_args->center = center;
 		dig_args->map = map;
-
+//
 		error = pthread_create(&center->scvs[center->scv - 1], NULL, dig, (void*) dig_args);
-
 		if (error != 0) {
 			perror("pthread_create");
 			return 1;
@@ -325,6 +332,8 @@ int train_scv(command_center_t *center, map_t *map) {
 		}
 
 		printf("SCV good to go, sir.\n");
+	} else {
+		printf("Not enough minerals.\n");
 	}
 
 	return 0;
